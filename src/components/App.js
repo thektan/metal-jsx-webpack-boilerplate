385c5ccaaa4e19df481303cd921b650482d9f6c9
Kevin Tan <kevin.tan@liferay.com>
2017-08-20 18:03:06 -0700

    Add metal-router with demo

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import JSXComponent from 'metal-jsx';
-import HelloWorld from './HelloWorld';
+import Router from 'metal-router';
+
+import Navbar from './Navbar';
+import Home from '../pages/Home';
+import About from '../pages/About';
+
 import '../css/App.scss';
 
 class App extends JSXComponent {
+	attached() {
+		Router.router().dispatch();
+	}
+
 	render() {
 		return (
 			<div id="app">
-				<HelloWorld />
+				<Navbar />
+
+				<Router component={Home} path="/" />
+				<Router component={About} path="/about" />
 			</div>
 		);
 	}
--- src/components/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-import JSXComponent from 'metal-jsx';
-
-class HelloWorld extends JSXComponent {
-	render() {
-		return <div>Hello, World!</div>;
-	}
-}
-
-export default HelloWorld;
